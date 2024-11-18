a21d6972734e5f74a1565dd42950b8f724748306
ryanzheng0507 <ryanzheng0507@gmail.com>
2024-11-18 09:45:26 +0000

    feat: outputs for new users and old users

--- number_guess.sh
+++ number_guess.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 PSQL="psql -X --username=freecodecamp --dbname=number_guess --tuples-only -c"
 
-# echo -e "\nEnter your username:"
-# read NAME
-# USER_ID=$($PSQL "SELECT user_id FROM users WHERE name = '$NAME'")
+echo -e "\nEnter your username:"
+read NAME
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE name = '$NAME'")
 
-# if [[ -z $USER_ID ]]
-# then
-# echo -e "\nWelcome, $NAME! It looks like this is your first time here."
-# fi
+if [[ -z $USER_ID ]]
+then
+echo -e "\nWelcome, $NAME! It looks like this is your first time here."
+NUMBER_GUESSING
+else
+GAME=$($PSQL "SELECT games_played FROM users WHERE name = '$NAME'")
+BEST=$($PSQL "SELECT best_game FROM users WHERE name = '$NAME'")
+echo -e "\nWelcome back, $NAME! You have played $GAMES games, and your best game took $BEST guesses."
+NUMBER_GUESSING
+fi
 
 NUMBER_GUESSING() {
   echo -e "\nGuess the secret number between 1 and 1000:"
@@ -32,5 +38,3 @@ NUMBER_GUESSING() {
   done
   echo -e "\nYou guessed it in $COUNT tries. The secret number was $RANDOM_NUMBER. Nice job!"
 }
-
-NUMBER_GUESSING
